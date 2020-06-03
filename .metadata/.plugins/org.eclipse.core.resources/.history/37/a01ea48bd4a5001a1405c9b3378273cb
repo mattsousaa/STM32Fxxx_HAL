395c9d81ba092fd1718465d0c2187da44dff0bb5
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-06-03 18:21:47 -0300

    HSE, HSI and PLL clock conf

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/37/a01ea48bd4a5001a1405c9b3378273cb
@@ -0,0 +1,8 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f1xx_hal.h"
+
+void Error_Handler(void);
+
+#endif /* __MAIN_H */
---
+++ USART2_Example_IT/Core/Inc/it.h
@@ -0,0 +1,13 @@
+/*
+ * it.h
+ *
+ *  Created on: May 31, 2020
+ *      Author: Mateus Sousa
+ */
+
+#ifndef INC_IT_H_
+#define INC_IT_H_
+
+
+
+#endif /* INC_IT_H_ */
