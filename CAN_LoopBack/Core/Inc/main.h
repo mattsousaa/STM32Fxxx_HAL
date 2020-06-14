7047e54e406ce88f5187634d68328b24e1efe258
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-06-14 17:06:15 -0300

    CAN loopback mode

---
+++ CAN_LoopBack/Core/Inc/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "stm32f4xx_hal.h"
+
+#define SYS_CLOCK_FREQ_50_MHZ 50
+#define SYS_CLOCK_FREQ_84_MHZ 84
+#define SYS_CLOCK_FREQ_120_MHZ 120
+
+#define TRUE 1
+#define FALSE 0
+
+#endif /* MAIN_H_ */
