f90f1a486162b2863c40e448312dab539c01fb30
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-06-16 16:47:04 -0300

    CAN normal mode with interrupts

---
+++ CAN_Normal_Mode_IT/Core/Inc/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "stm32f4xx_hal.h"
+
+#define SYS_CLOCK_FREQ_50_MHZ 50
+#define SYS_CLOCK_FREQ_84_MHZ 84
+#define SYS_CLOCK_FREQ_120_MHZ 120
+
+
+#define LED1_PORT GPIOC
+#define LED2_PORT GPIOC
+#define LED3_PORT GPIOB
+#define LED4_PORT GPIOC
+
+#define LED1_PIN_NO GPIO_PIN_9
+#define LED2_PIN_NO GPIO_PIN_8
+#define LED3_PIN_NO GPIO_PIN_8
+#define LED4_PIN_NO GPIO_PIN_6
+
+#define TRUE 1
+#define FALSE 0
+
+#endif /* MAIN_H_ */
