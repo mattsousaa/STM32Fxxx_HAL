9b5c0dfba7be8d40588bfb90bc3dac94fa1cddd5
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-06-26 19:41:33 -0300

    Current measurement with CoreMark

--- CURRENT_MEASUREMENT_RUN_MODE/Debug/startup/subdir.mk
+++ CURRENT_MEASUREMENT_RUN_MODE/Debug/startup/subdir.mk
@@ -12,5 +12,5 @@ OBJS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 startup/%.o: ../startup/%.s
-	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -c -I"C:/Users/mateu/Documents/Curso STM32/MCU2/CURRENT_MEASUREMENT_RUN_MODE/StdPeriph_Driver/inc" -I"C:/Users/mateu/Documents/Curso STM32/MCU2/CURRENT_MEASUREMENT_RUN_MODE/inc" -I"C:/Users/mateu/Documents/Curso STM32/MCU2/CURRENT_MEASUREMENT_RUN_MODE/CMSIS/device" -I"C:/Users/mateu/Documents/Curso STM32/MCU2/CURRENT_MEASUREMENT_RUN_MODE/CMSIS/core" -x assembler-with-cpp --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -c -I"C:/Users/mateu/Documents/Curso STM32/MCU2/Current_Measurement_Run_Mode/StdPeriph_Driver/inc" -I"C:/Users/mateu/Documents/Curso STM32/MCU2/Current_Measurement_Run_Mode/inc" -I"C:/Users/mateu/Documents/Curso STM32/MCU2/Current_Measurement_Run_Mode/CMSIS/device" -I"C:/Users/mateu/Documents/Curso STM32/MCU2/Current_Measurement_Run_Mode/CMSIS/core" -x assembler-with-cpp --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
