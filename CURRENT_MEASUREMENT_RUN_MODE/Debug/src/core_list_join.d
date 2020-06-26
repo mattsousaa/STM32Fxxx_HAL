8a7278f1165fa9f8baecc73d6d74d0998bb79e49
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-06-26 19:50:46 -0300

    Current measurement with CoreMark

--- CURRENT_MEASUREMENT_RUN_MODE/Debug/src/core_list_join.d
+++
@@ -1,7 +0,0 @@
-src/core_list_join.o: ../src/core_list_join.c \
- C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/coremark.h \
- C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/core_portme.h
-
-C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/coremark.h:
-
-C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/core_portme.h:
--- CURRENT_MEASUREMENT_RUN_MODE/Debug/src/core_state.d
+++
@@ -1,7 +0,0 @@
-src/core_state.o: ../src/core_state.c \
- C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/coremark.h \
- C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/core_portme.h
-
-C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/coremark.h:
-
-C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/core_portme.h:
