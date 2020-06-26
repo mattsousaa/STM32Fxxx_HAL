3e3fa7b997c5e86d2d77a9b34deb4452ff782c35
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-06-26 19:10:27 -0300

    Current measurement with CoreMark

---
+++ CURRENT_MEASUREMENT_RUN_MODE/Debug/src/core_matrix.d
@@ -0,0 +1,7 @@
+src/core_matrix.o: ../src/core_matrix.c \
+ C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/coremark.h \
+ C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/core_portme.h
+
+C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/coremark.h:
+
+C:/Users/mateu/Documents/MasteringMCU2-master/Source_codes/OpenStm32SWB/MasteringMCU2/Current_Measurement_Run_Mode/inc/core_portme.h:
