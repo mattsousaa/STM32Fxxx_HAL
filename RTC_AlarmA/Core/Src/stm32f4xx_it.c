9448e2f20fa524a181caab81735f12c05c8120b2
mattsousaa <mateuseng_ec@alu.ufc.br>
2020-07-04 17:37:30 -0300

    RTC Alarm with interrupts

---
+++ RTC_AlarmA/Core/Src/stm32f4xx_it.c
@@ -0,0 +1,23 @@
+/*
+ * stm32f4xx_it.c
+ *
+ *  Created on: 04 de Julho de 2020
+ *      Author: Mateus Sousa
+ */
+
+#include "main.h"
+
+extern RTC_HandleTypeDef hrtc;
+
+void SysTick_Handler(void){
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
+void EXTI15_10_IRQHandler(void){
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_13);
+}
+
+void RTC_Alarm_IRQHandler(void){
+	HAL_RTC_AlarmIRQHandler(&hrtc);
+}
